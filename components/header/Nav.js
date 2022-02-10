e429a7e5f0116960cdaeda9c2ed792a5daa58f80
Gregory Sanchez <sanchez.gregory886@gmail.com>
2022-02-09 18:01:23 -0800

    Add NavDrawer to mobile mode

--- components/header/Nav.js
+++ components/header/Nav.js
@@ -1,18 +1,31 @@
 import {useSelector} from 'react-redux';
 import {styled} from '@mui/material/styles';
-import {AppBar, Container, Toolbar, Typography} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ClearIcon from '@mui/icons-material/Clear';
+import {AppBar, Button, Container, Toolbar, Typography} from '@mui/material';
 
 import NavLink from './NavLink';
 import siteLinks from '../../lib/utils/siteLinks';
 import {getMobile} from '../../lib/redux/selectors';
+import {globalOptions} from '../../lib/utils/emotionStyled';
 
 
 const HeaderBar = styled(AppBar)`
   box-shadow: none;
   z-index: 10;
+  
+  height: 100%;
+  justify-content: center;
 `;
 
-export default function Nav({}){
+const Menu = styled(Button, globalOptions)`
+  transition: 0.3s;
+  height: 100px;
+  width: 100px;
+  color: ${props => props.theme.palette.textOpposite.primary};
+`;
+
+export default function Nav({drawerOpen, toggleDrawer}){
     const mobile = useSelector(getMobile);
 
     return (
@@ -21,18 +34,27 @@ export default function Nav({}){
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
                         <NavLink path="/" active={false}>
-                            <Typography variant={mobile ? "h3" : "h5"}>
+                            <Typography variant="h3">
                                 EF
                             </Typography>
                         </NavLink>
 
                         {!mobile && siteLinks.map(({text, ...rest}) => (
                             <NavLink {...rest}>
-                                <Typography variant={mobile ? "h4" : "body1"}>
+                                <Typography variant="h4">
                                     {text}
                                 </Typography>
                             </NavLink>
                         ))}
+                        {mobile && (
+                            <Menu onClick={toggleDrawer}>
+                                {drawerOpen ? (
+                                    <ClearIcon fontSize="large"/>
+                                ) : (
+                                    <MenuIcon fontSize="large"/>
+                                )}
+                            </Menu>
+                        )}
                     </Toolbar>
                 </Container>
             </HeaderBar>
---
+++ components/header/NavDrawer.js
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+import {useSelector} from 'react-redux';
+import {Drawer, Typography} from '@mui/material';
+
+import NavLink from './NavLink';
+import siteLinks from '../../lib/utils/siteLinks';
+import {getDarkMode} from '../../lib/redux/selectors';
+import {globalOptions} from '../../lib/utils/emotionStyled';
+
+const TopDrawer = styled(Drawer)`
+  .MuiDrawer-paper {
+    height: 100%;
+  }
+`;
+
+const DrawerContainer = styled('div', globalOptions)`
+  height: 100%;
+  padding-top: 25vh;
+  padding-bottom: 5%;
+  background-color: ${props => props.theme.palette.background.paper};
+  transition: 0.3s;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  overflow-y: auto;
+`;
+
+const DrawerLink = styled(NavLink)`
+  flex: 1;
+`;
+
+export default function NavDrawer({drawerOpen, closeDrawer}) {
+    const darkMode = useSelector(getDarkMode);
+
+
+    return (
+        <>
+            <TopDrawer
+                open={drawerOpen}
+                onClose={closeDrawer}
+                anchor="top"
+                transitionDuration={350}
+            >
+                <DrawerContainer data-dm={darkMode}>
+                    {siteLinks.map(({text, ...rest}) => (
+                        <DrawerLink active={false} onClick={closeDrawer} {...rest} >
+                            <Typography variant="h6">
+                                {text}
+                            </Typography>
+                        </DrawerLink>
+                    ))}
+                </DrawerContainer>
+            </TopDrawer>
+        </>
+    );
+}
